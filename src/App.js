6dd05db854a54eaa8593ced886aff2272d7ca809
KPVBY <kpvbygm@gmail.com>
2019-12-10 01:49:35 +0300

    added page structure and scss files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import Header from "./components/header";
+import Content from "./components/main";
+import Footer from "./components/footer";
+import "./app.scss"
 
 export default class App extends Component{
   render() {
     return (
         <div className="App">
-
+            <Header/>
+            <Content/>
+            <Footer/>
         </div>
     );
   }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+import "./footer.scss"
+export default class Footer extends Component{
+    render() {
+        return(
+            <footer className="app-footer">
+                Footer
+            </footer>
+        )
+    }
+}
